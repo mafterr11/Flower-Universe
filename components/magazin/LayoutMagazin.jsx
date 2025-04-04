c150594550a8a3637c8b12cefd3e58c16b407966
mafterr11 <mafterr11@gmail.com>
2025-04-04 22:08:07 +0300

    .

--- components/magazin/LayoutMagazin.jsx
+++ components/magazin/LayoutMagazin.jsx
@@ -3,7 +3,7 @@ export default function LayoutMagazin({ title, description, children }) {
     <div className="relative mx-auto mb-44 flex max-w-5xl flex-col items-center px-4 pt-[12rem] text-center text-white">
       {/* Decorative ornament */}
       <div className="bg-hero pointer-events-none fixed inset-0 z-[-1] bg-cover bg-no-repeat">
-        <div className="absolute inset-0 bg-black/40" />
+        <div className="absolute inset-0 bg-black/35" />
       </div>
 
       <h1 className="text-4xl leading-8">{title}</h1>
