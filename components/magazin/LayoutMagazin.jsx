381e94ca03c1c07cf36709a68fef7cb130dffda1
Alexandru Maftei <mafterr11@gmail.com>
2025-04-08 14:13:57 +0300

    .

--- components/magazin/LayoutMagazin.jsx
+++ components/magazin/LayoutMagazin.jsx
@@ -2,7 +2,7 @@ export default function LayoutMagazin({ title, description, children, className 
   return (
     <div className={`relative mx-auto mb-44 flex max-w-5xl flex-col items-center px-4 max-md:pt-[8.5rem] pt-[12rem] text-center text-white ${className}`}>
       {/* Decorative ornament */}
-      <div className="bg-magazin pointer-events-none fixed inset-0 z-[-1] bg-cover bg-no-repeat">
+      <div className="bg-magazin pointer-events-none fixed inset-0 z-[-1] bg-cover bg-no-repeat bg-scroll md:bg-fixed">
         <div className="absolute inset-0 bg-black/30" />
       </div>
 
